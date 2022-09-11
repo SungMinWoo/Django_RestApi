bddea885b19a983a19af5afb39a4c5a1f1ee19f2
성민우 <okekdma2@gmail.com>
2022-09-11 23:53:23 +0900

    Django Rest FrameWork tutorial make api app and DRF setting

--- django-restapi/MyProject/api/urls.py
+++ django-restapi/MyProject/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import PostView
+from .views import PostView, article_list
 
 
 post_list = PostView.as_view({
@@ -20,4 +20,5 @@ urlpatterns = format_suffix_patterns([
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('posts/', post_list, name='post_list'),
     path('posts/<int:pk>/', post_detail, name='post_detail'),
+    path('article/', article_list),
 ])
--- django-restapi/MyProject/api/views.py
+++ django-restapi/MyProject/api/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework import permissions
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
 
 
 class PostView(viewsets.ModelViewSet):
@@ -11,3 +13,29 @@ class PostView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+def article_list(request, pk):
+    """
+    Retrieve, update or delete article.
+    """
+    try:
+        article = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = PostSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(article, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)
